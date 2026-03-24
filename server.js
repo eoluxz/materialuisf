3ce3d1eed57babfb57b0fc72f46be978270e4bbf
Lab05_etec <Lab05_etec@etec>
2026-03-24 16:38:53 -0300

    ndfgng

--- server.js
+++ server.js
@@ -10,6 +10,38 @@ app.get("/", (request, response) => {
     response.send("Olá mundo!");
 });
 
+app.get("/usuario", (request, response) => {
+    response.json({
+        id: 1,
+        nome: "Lucca Machado",
+        profissao: "coletador de galinha"
+
+    });
+});
+
+
+app.get("/usuarios", (request, response) => {
+    response.json([
+        {
+            id: 1,
+            nome: "Livia",
+            profissao: "biomedica"
+        },
+        {
+            id: 2,
+            nome: "Mark",
+            profissao: "Dono de cabaré"
+
+        },
+        {
+            id: 3,
+            nome: "Pedro",
+            profissao: "Streamer"
+
+        }
+    ]);
+});
+
 app.listen(porta, (request, response) => {
     console.log(`Servidor rodando: http://127.0.0.1:${porta}`)
 });
